6c839b0bc4d5541e7689cc0e7d362c78ac18c654
dustin kohls <s0579625@htw-berlin.de>
2023-05-15 11:50:15 +0200

    Added React-Webcam

--- src/components/MPHands.tsx
+++ src/components/MPHands.tsx
@@ -8,7 +8,7 @@ import { Camera } from "@mediapipe/camera_utils";
 import * as cam from "@mediapipe/camera_utils"; //notwendig?
 import { drawConnectors } from "@mediapipe/drawing_utils";
 import { drawLandmarks } from "@mediapipe/drawing_utils";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import {
   GestureRecognizer,
   FilesetResolver,
@@ -19,16 +19,10 @@ let gestureRecognizer: any;
 let enableWebcamButton;
 let webcamRunning = true;
 let canvasCtx: any;
-const videoHeight = "360px";
-const videoWidth = "480px";
 
 const MPHands = () => {
   const webcamRef = useRef<Webcam | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  // Before we can use HandLandmarker class we must wait for it to finish
-  // loading. Machine Learning models can be large and take a moment to
-  // get everything needed to run.
   const createGestureRecognizer = async () => {
     const vision = await FilesetResolver.forVisionTasks(
       "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.1.0-alpha-13/wasm"
@@ -42,116 +36,130 @@ const MPHands = () => {
     });
   };
   createGestureRecognizer();
-  console.log("nach init GestureRec");
 
-  setTimeout(enableCam, 3000);
-  console.log("nach Timeout");
-  /********************************************************************
-  // Demo 2: Continuously grab image from webcam stream and detect it.
-  ********************************************************************/
-
-  const video = document.getElementById("webcam") as HTMLVideoElement;
-  const canvasElement = document.getElementById(
-    "output_canvas"
-  ) as HTMLCanvasElement;
-  if (canvasElement) {
-    const canvasCtx = canvasElement.getContext("2d");
-  }
-
-  const gestureOutput = document.getElementById("gesture_output");
-
-  // Check if webcam access is supported.
-  function hasGetUserMedia() {
-    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-  }
-  console.log("nach deklaraitons");
-
-  // Enable the live webcam view and start detection.
-  function enableCam() {
-    console.log("Start EnableCAM");
-    if (!gestureRecognizer) {
-      alert("Please wait for gestureRecognizer to load");
-      return;
-    }
-    // getUsermedia parameters.
-    const constraints = {
-      video: true,
-    };
-
-    // Activate the webcam stream.
-    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-      video.srcObject = stream;
-      video.addEventListener("loadeddata", predictWebcam);
+  useEffect(() => {
+    const hands = new Hands({
+      locateFile: (file) => {
+        console.log(`${file}`);
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
     });
-  }
+    hands.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.8,
+      minTrackingConfidence: 0.5,
+    });
+    hands.onResults(onResults);
 
-  async function predictWebcam() {
-    const webcamElement = document.getElementById("webcam");
-    // Now let's start detecting the stream.
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video !== null
+    ) {
+      const camera = new Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          const video = webcamRef.current?.video;
+          if (video) {
+            await hands.send({ image: video });
+          }
+        },
+        width: 360,
+        height: 640,
+      });
+      camera.start();
+    }
+  }, []);
 
+  const onResults = (results: Results) => {
+    const video = webcamRef.current?.video;
+    const canvasElement = canvasRef.current;
+    if (!canvasElement) return;
+    const canvasCtx = canvasElement.getContext("2d");
+    if (!canvasCtx) return;
+
+    const videoWidth = video?.videoWidth;
+    const videoHeight = video?.videoHeight;
+    if (!videoWidth || !videoHeight) return;
+
+    // Set canvas width
+    canvasElement.width = videoWidth;
+    canvasElement.height = videoHeight;
+
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, videoWidth, videoHeight);
+    canvasCtx.translate(videoWidth, 0);
+    canvasCtx.scale(-1, 1);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    if (results.multiHandLandmarks) {
+      console.log("Found Hand");
+      for (const landmarks of results.multiHandLandmarks) {
+        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+          color: "#00FF00",
+          lineWidth: 5,
+        });
+        drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 });
+      }
+    }
+    canvasCtx.restore();
     let nowInMs = Date.now();
-    const results = gestureRecognizer.recognizeForVideo(video, nowInMs);
-    console.log("Predicten durch");
+    const resultsRec = gestureRecognizer.recognizeForVideo(video, nowInMs);
+    console.log("Prediction durhc");
 
-    //Anzeigen der Keypoints START
-    // canvasCtx.save();
-    // canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-
-    // if (webcamElement) {
-    //   webcamElement.style.width = videoWidth;
-    //   webcamElement.style.height = videoHeight;
-    // }
-
-    // canvasElement.style.height = videoHeight;
-
-    // canvasElement.style.width = videoWidth;
-
-    // if (results.landmarks) {
-    //   for (const landmarks of results.landmarks) {
-    //     drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
-    //       color: "#00FF00",
-    //       lineWidth: 5,
-    //     });
-    //     drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 });
-    //   }
-    // }
-    // canvasCtx.restore();
-    //Anzeigen der Keypoints ENDE
-
-    if (results.gestures.length > 0) {
+    if (resultsRec.gestures.length > 0) {
       // gestureOutput.style.display = "block";
-      const categoryName = results.gestures[0][0].categoryName;
-      console.log(`Predicition: ${categoryName}`);
-      const categoryScore = (results.gestures[0][0].score * 100).toFixed(2);
+      const categoryName = resultsRec.gestures[0][0].categoryName;
+      console.log(`Predicition ${categoryName}`);
+
+      const categoryScore = (resultsRec.gestures[0][0].score * 100).toFixed(2);
+
       const gestureOutput = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %`;
       console.log(`${gestureOutput}`);
-
-      // const categoryScore = parseFloat(
-      //   results.gestures[0][0].score * 100
-      // ).toFixed(2);
-      // gestureOutput.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %`;
+      //setGestureOutputText(gestureOutput);
     } else {
+      //setGestureOutputText("None");
       // gestureOutput.style.display = "none";
     }
-    // Call this function again to keep predicting when the browser is ready.
-    if (webcamRunning === true) {
-      window.requestAnimationFrame(predictWebcam);
-    }
-  }
+  };
 
   return (
     <div>
-      <div style={{ position: "relative" }}>
-        <video id="webcam" autoPlay playsInline></video>
-        <canvas
-          className="output_canvas"
-          id="output_canvas"
-          width="1280"
-          height="720"
-          style={{ position: "absolute", left: "0px", top: "0px" }}
-        ></canvas>
-        <p id="gesture_output" className="output"></p>
-      </div>
+      <Webcam
+        audio={false}
+        mirrored={true}
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: "0",
+          right: "0",
+          textAlign: "center",
+          zIndex: 9,
+          width: 360,
+          height: 640,
+        }}
+      ></Webcam>
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: "0",
+          right: "0",
+          textAlign: "center",
+          zIndex: 9,
+          width: 360,
+          height: 640,
+        }}
+      ></canvas>
     </div>
   );
 };
